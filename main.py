0be47a777a432eb8c55ab17d16051a24739c9dcc
PSYSHO <giks1998@mail.ru>
2022-10-22 21:15:46 +0400

    SmallUniversitySystem

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-from os.path import exists
-
-from CSVcreator import creating
-from fileWorker import writing_scv
-from fileWorker import writing_txt
+import student as st
+import lecture as lect
 
 
-path = 'Phonebook.csv'
-valid = exists(path)
-if not valid:
-    creating()
-writing_scv()
-writing_txt()
+def option():
+    print("\nВас приветствует программа мониторинга процессов обучения студентов!")
+    ch = int(input('Введите что хотите сделать: \n \
+    1: Поиск ID студента по фамилии \n \
+    2: Посмотреть класс  и место которое занимает  студент \n \
+    3: Выход\n \
+    Ваш выбор: '))
+
+    if ch == 1:
+        Surn = str(input("Введите фамилию ученика: "))
+        if Surn in st.students['Фамилия']:
+            index = st.students['Фамилия'].index(Surn)
+        print(f"{st.students['ID'][index]}, {st.students['Имя'][index]},{st.students['Фамилия'][index]}\n,{st.students['Дата рождения'][index]}, {st.students['Успеваемость'][index]}")
+        print('\nХотите выполнить какую-то другую операцию??? Y или N: ')
+        num = input()
+        if num == 'Y' or 'y' or 'У' or 'у':
+            option()
+        exit()
+
+    elif ch == 2:
+        c = input('Введите ID студента для вывода по классам: ')
+        if c in lect.lecture['ID']:
+            index = lect.lecture['ID'].index(c)
+            print(f" Сидит в классе - {lect.lecture['Предмет'][index]}\n\, за партой номер - {lect.lecture['Номер парты'][index]}, это {lect.lecture['Ряд'][index]}, парта - {lect.lecture['Вид парты'][index]}, Имя: {st.students['Имя'][index]}, Фамилия - {st.students['Фамилия'][index]}, и успеваемасть у студента: {st.students['Успеваемость'][index]}")
+            print('\nХотите выполнить другую операцию??? Y или N: ')
+            num = input()
+            if num == 'Y' or 'y' or 'У' or 'у':
+                option()
+            exit()
+    else:
+        print('еще раз')
+    exit()
+
+
+option()
